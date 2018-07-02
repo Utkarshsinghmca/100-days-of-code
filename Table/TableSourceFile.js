a06fd9afb405143328eac52c45b497e39b7a9bce
Utkarsh Singh <30746921+Utkarshsinghmca@users.noreply.github.com>
2018-07-02 09:41:06 +0530

    Day 0.2

---
+++ Table/TableSourceFile.js
@@ -0,0 +1,16 @@
+var tb;
+document.write("<table border='30px'>");
+
+for(var u = 1; u < 10; u++) {
+
+	document.write("<tr style='height:40px;'>");
+	for(var s = 1; s < 11; s++)
+	{
+		
+	if(s == 1 || u == 1){}
+	else{}
+	
+	document.write("<td style='width:100px;background-color:" + tb + "'>" + u*s + "</td>");
+	}
+}
+
